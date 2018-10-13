fbddfe2abff3276a54705fecc11da6c158e1eaff
Anna <a.karlysheva@ukma.edu.ua>
2018-10-13 23:36:02 +0300

    normal SCMemberRepositoryTest

--- src/test/java/ua/edu/ukma/e_oss/dao/SСMemberRepositoryTest.java
+++ src/test/java/ua/edu/ukma/e_oss/dao/SСMemberRepositoryTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ScMemberRepositoryTest {
+class SСMemberRepositoryTest {
 
     @Test
     void save() {
