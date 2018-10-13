e51a5d742a3d05dca13354c02edf99082635283c
fedus <fedusov.serhiy@gmail.com>
2018-10-13 22:31:06 +0300

    fixed controller

--- src/main/java/ua/edu/ukma/e_oss/controller/MainLogicController.java
+++ src/main/java/ua/edu/ukma/e_oss/controller/MainLogicController.java
@@ -1,14 +1,15 @@
 package ua.edu.ukma.e_oss.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainLogicController {
 
     @GetMapping("/ticket")
     private String getTicketPage(Model model){
+
         return "ticket";
     }
 
