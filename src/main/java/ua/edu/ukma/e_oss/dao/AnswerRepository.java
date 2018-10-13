117361aeb6842fc5a2438b01e65118cf4ef805d3
fedus <fedusov.serhiy@gmail.com>
2018-10-13 21:44:13 +0300

    created template for tests

--- src/main/java/ua/edu/ukma/e_oss/dao/AnswerRepository.java
+++ src/main/java/ua/edu/ukma/e_oss/dao/AnswerRepository.java
@@ -3,5 +3,39 @@ package ua.edu.ukma.e_oss.dao;
 import org.springframework.data.repository.CrudRepository;
 import ua.edu.ukma.e_oss.model.Answer;
 
+import java.util.Optional;
+
 public interface AnswerRepository extends CrudRepository<Answer, Integer> {
+    @Override
+    <S extends Answer> S save(S s);
+
+    @Override
+    <S extends Answer> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    Optional<Answer> findById(Integer integer);
+
+    @Override
+    boolean existsById(Integer integer);
+
+    @Override
+    Iterable<Answer> findAll();
+
+    @Override
+    Iterable<Answer> findAllById(Iterable<Integer> iterable);
+
+    @Override
+    long count();
+
+    @Override
+    void deleteById(Integer integer);
+
+    @Override
+    void delete(Answer answer);
+
+    @Override
+    void deleteAll(Iterable<? extends Answer> iterable);
+
+    @Override
+    void deleteAll();
 }
--- src/main/java/ua/edu/ukma/e_oss/dao/ScMemberRepository.java
+++ src/main/java/ua/edu/ukma/e_oss/dao/ScMemberRepository.java
@@ -3,5 +3,39 @@ package ua.edu.ukma.e_oss.dao;
 import org.springframework.data.repository.CrudRepository;
 import ua.edu.ukma.e_oss.model.SCMember;
 
+import java.util.Optional;
+
 public interface ScMemberRepository extends CrudRepository<SCMember, Integer> {
+    @Override
+    <S extends SCMember> S save(S s);
+
+    @Override
+    <S extends SCMember> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    Optional<SCMember> findById(Integer integer);
+
+    @Override
+    boolean existsById(Integer integer);
+
+    @Override
+    Iterable<SCMember> findAll();
+
+    @Override
+    Iterable<SCMember> findAllById(Iterable<Integer> iterable);
+
+    @Override
+    long count();
+
+    @Override
+    void deleteById(Integer integer);
+
+    @Override
+    void delete(SCMember scMember);
+
+    @Override
+    void deleteAll(Iterable<? extends SCMember> iterable);
+
+    @Override
+    void deleteAll();
 }
