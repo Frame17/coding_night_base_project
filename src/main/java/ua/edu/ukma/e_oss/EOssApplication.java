fd534a3ead0bfcbbd8cde18df52ef0f0756f564e
fedus <fedusov.serhiy@gmail.com>
2018-10-13 17:46:36 +0300

    added project structure

--- src/main/java/ua/edu/ukma/e_oss/EOssApplication.java
+++ src/main/java/ua/edu/ukma/e_oss/EOssApplication.java
@@ -1,4 +1,4 @@
-package ua.edu.ukma.e_oss.config;
+package ua.edu.ukma.e_oss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
