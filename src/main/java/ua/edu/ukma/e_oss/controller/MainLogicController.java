77db8016eb57e71f91b0d23ab240f052b24fceba
fedus <fedusov.serhiy@gmail.com>
2018-10-13 23:44:08 +0300

    Interaction with model

--- src/main/java/ua/edu/ukma/e_oss/controller/MainLogicController.java
+++ src/main/java/ua/edu/ukma/e_oss/controller/MainLogicController.java
@@ -1,14 +1,32 @@
 package ua.edu.ukma.e_oss.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ua.edu.ukma.e_oss.model.SCMember;
+import ua.edu.ukma.e_oss.model.Ticket;
+import ua.edu.ukma.e_oss.service.SCMemberService;
+import ua.edu.ukma.e_oss.service.TicketService;
+
+import java.util.List;
 
 @Controller
 public class MainLogicController {
 
+    private final SCMemberService scMemberService;
+    private final TicketService ticketService;
+
+    @Autowired
+    public MainLogicController(SCMemberService scMemberService, TicketService ticketService) {
+        this.scMemberService = scMemberService;
+        this.ticketService = ticketService;
+    }
+
     @GetMapping("/ticket")
     private String getTicketPage(Model model){
+        Iterable<SCMember> SCmembers = scMemberService.findAll();
+        model.addAttribute("SCmembers", SCmembers);
 
         return "ticket";
     }
@@ -20,6 +38,8 @@ public class MainLogicController {
 
     @GetMapping("/mainPage")
     private String getMainPage(Model model) {
-        return "TicketsPage";
+        Iterable<Ticket> tickets = ticketService.findAll();
+        model.addAttribute("tickets", tickets);
+        return "tickets";
     }
 }
