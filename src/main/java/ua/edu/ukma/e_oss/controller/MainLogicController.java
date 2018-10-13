72a52cc02a34d709334fd9acc5163b95dc3973ec
fedus <fedusov.serhiy@gmail.com>
2018-10-14 01:33:45 +0300

    passing arguments to model

--- src/main/java/ua/edu/ukma/e_oss/controller/MainLogicController.java
+++ src/main/java/ua/edu/ukma/e_oss/controller/MainLogicController.java
@@ -1,33 +1,46 @@
 package ua.edu.ukma.e_oss.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ua.edu.ukma.e_oss.model.Answer;
 import ua.edu.ukma.e_oss.model.SCMember;
 import ua.edu.ukma.e_oss.model.Ticket;
+import ua.edu.ukma.e_oss.service.AnswerService;
 import ua.edu.ukma.e_oss.service.SCMemberService;
 import ua.edu.ukma.e_oss.service.TicketService;
 
-import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MainLogicController {
 
     private final SCMemberService scMemberService;
     private final TicketService ticketService;
+    private final AnswerService answerService;
 
     @Autowired
-    public MainLogicController(SCMemberService scMemberService, TicketService ticketService) {
+    public MainLogicController(SCMemberService scMemberService, TicketService ticketService, AnswerService answerService) {
         this.scMemberService = scMemberService;
         this.ticketService = ticketService;
+        this.answerService = answerService;
     }
 
     @GetMapping("/ticket")
-    private String getTicketPage(Model model){
+    private String getTicketPage(@RequestParam(name = "id") int id, Model model) {
         Iterable<SCMember> SCmembers = scMemberService.findAll();
-        model.addAttribute("SCmembers", SCmembers);
+        Optional<Ticket> optionalTicket = ticketService.findById(id);
+        if (!optionalTicket.isPresent())
+            throw new ResourceNotFoundException();
+        Ticket ticket = optionalTicket.get();
+        Iterable<Answer> answers = answerService.findAllByTicket(ticket);
 
+        model.addAttribute("ticket", ticket);
+        model.addAttribute("SCmembers", SCmembers);
+        model.addAttribute("answers", answers);
         return "ticket";
     }
 
