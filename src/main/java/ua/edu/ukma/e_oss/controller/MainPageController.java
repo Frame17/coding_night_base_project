8ac7839a36ef0966144cdb450ae0e6e96bfaedd2
fedus <fedusov.serhiy@gmail.com>
2018-10-13 21:12:59 +0300

    created custom mappings

--- src/main/java/ua/edu/ukma/e_oss/controller/MainPageController.java
+++ src/main/java/ua/edu/ukma/e_oss/controller/MainPageController.java
@@ -1,24 +1,24 @@
 package ua.edu.ukma.e_oss.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainPageController {
 
     @GetMapping("/ticket")
-    private String getTicketPage(Model model){
+    public String getTicketPage(Model model) {
         return "ticket";
     }
 
     @GetMapping("/userPage")
-    private String getUserPage(Model model) {
+    public String getUserPage(Model model) {
         return "";  // todo - implement
     }
 
     @GetMapping("/mainPage")
-    private String getMainPage(Model model) {
+    public String getMainPage(Model model) {
         return "TicketsPage";
     }
 }
