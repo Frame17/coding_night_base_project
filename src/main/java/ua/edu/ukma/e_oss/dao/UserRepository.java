a420b2732f5c4aa96f9450552f326c5afc2f69ab
Anna <a.karlysheva@ukma.edu.ua>
2018-10-13 21:48:04 +0300

    ticketRep and userRep basic methods + ticketRep modify queries. ticketService editing ticket methods

--- src/main/java/ua/edu/ukma/e_oss/dao/UserRepository.java
+++ src/main/java/ua/edu/ukma/e_oss/dao/UserRepository.java
@@ -3,7 +3,31 @@ package ua.edu.ukma.e_oss.dao;
 import org.springframework.data.repository.CrudRepository;
 import ua.edu.ukma.e_oss.model.User;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+    @Override
+    <S extends User> S save(S s);
 
+    @Override
+    <S extends User> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    Optional<User> findById(Integer integer);
+
+    @Override
+    Iterable<User> findAll();
+
+    @Override
+    void deleteById(Integer integer);
+
+    @Override
+    void delete(User user);
+
+    @Override
+    void deleteAll(Iterable<? extends User> iterable);
+
+    @Override
+    void deleteAll();
 }
--- src/main/java/ua/edu/ukma/e_oss/service/TicketService.java
+++ src/main/java/ua/edu/ukma/e_oss/service/TicketService.java
@@ -3,6 +3,7 @@ package ua.edu.ukma.e_oss.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.edu.ukma.e_oss.dao.TicketRepository;
+import ua.edu.ukma.e_oss.model.SCMember;
 import ua.edu.ukma.e_oss.model.Ticket;
 
 @Service
@@ -14,6 +15,18 @@ public class TicketService {
     Ticket save(Ticket ticket){
         Ticket savedTicked = ticketRepository.save(ticket);
         return savedTicked;
+    }
+
+    public SCMember changeSCid(SCMember scMember, Ticket ticket){
+        int ticketID = ticket.getId();
+        SCMember newSCMember = ticketRepository.updateSCidFor(scMember, ticketID);
+        return newSCMember;
+    }
+
+    public byte changeStatus(byte status, Ticket ticket){
+        int ticketID = ticket.getId();
+        byte newStatus = ticketRepository.updateStatus(status, ticketID);
+        return newStatus;
     }
 
 
