2b94309a911aacddb8620808a6e383b28b1f7f79
fedus <fedusov.serhiy@gmail.com>
2018-10-23 00:07:35 +0300

    removed unnecessary component scan specification

--- src/main/java/ua/edu/ukma/e_oss/EOssApplication.java
+++ src/main/java/ua/edu/ukma/e_oss/EOssApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "ua.edu.ukma.e_oss.controller","ua.edu.ukma.e_oss.config", "ua.edu.ukma.e_oss.service"})
 public class EOssApplication {
 
 	public static void main(String[] args) {
