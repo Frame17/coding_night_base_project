c9bd28b7b20a4e2777b091ec4b17d9a8d120dd05
fedus <fedusov.serhiy@gmail.com>
2018-10-27 00:16:28 +0300

    login and redirection tests

--- src/test/java/ua/edu/ukma/e_oss/controller/MainPageControllerTest.java
+++ src/test/java/ua/edu/ukma/e_oss/controller/MainPageControllerTest.java
@@ -6,13 +6,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import javax.servlet.Filter;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -22,29 +25,48 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class MainPageControllerTest {
 
     @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
     private MockMvc mvc;
 
     @Before
-    public void setUp() throws Exception {
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
+    @WithMockUser("user1")
+    @Test
+    public void getUserPage_mustSucceedWith200() throws Exception {
+        MvcResult mvcResult = mvc
+                .perform(get("/userPage"))
+                .andExpect(status().isOk())
+                .andReturn();
+        // todo verify context
     }
 
     @Test
-    public void getUserPage() throws Exception {
-        // todo - bypass the login page
-//        mvc
-//                .perform(get("/userPage"))
-//                .andExpect(status().isOk());
+    public void getUserPage_mustRedirectOnLogin() throws Exception {
+        mvc
+                .perform(get("/userPage"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
     }
 
     @Test
     public void getMainPage() throws Exception {
-        mvc
+        MvcResult mvcResult = mvc
                 .perform(get("/mainPage"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andReturn();
+        //System.out.println(mvcResult.getResponse().getContentAsString());       // todo parse?
     }
 
     @Test
     public void postMainPage() {
+
     }
 }
